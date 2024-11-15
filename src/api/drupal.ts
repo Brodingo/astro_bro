c84c54a8da97a4e92b49960c44d7613791acfcda
Brodingo <brodyk@gmail.com>
2024-11-14 22:23:00 -0500

    handler for getting article nodes

--- src/api/drupal.ts
+++ src/api/drupal.ts
@@ -13,9 +13,28 @@ export const baseURL: string = import.meta.env.DRUPAL_BASE_URL;
  * 
  * @return Promise<TJsonaModel | TJsonaModel[]> as Promise<any>
  */
-export const fetchUurl = async (url: string): Promise<any> => {
+export const fetchUrl = async (url: string): Promise<any> => {
     const request: Response = await fetch(url);
     const json: string | TJsonApiBody = await request.json();
     const dataFormatter: Jsona = new Jsona();
     return dataFormatter.deserialize(json);
+}
+
+/**
+ * Get all published articles
+ * 
+ * @return Promise<DrupalNode[]>
+ */
+export const getArticles = async (): Promise<DrupalNode[]> => {
+    const params: DrupalJsonApiParams = new DrupalJsonApiParams();
+    params
+        .addFields("node--article", [
+            "title",
+            "path",
+            "body",
+            "created",
+        ])
+        .addFilter("status", "1");
+    const path: string = params.getQueryString();
+    return await fetchUrl(baseUrl + '/jsonapi/node/article?' + path);
 }
