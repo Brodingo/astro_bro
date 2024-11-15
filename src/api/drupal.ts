fb33b5e060811808c068601d5755a1c3b544fe60
Brodingo <brodyk@gmail.com>
2024-11-14 22:27:41 -0500

    landing page for articles

--- src/api/drupal.ts
+++ src/api/drupal.ts
@@ -4,7 +4,7 @@ import type { DrupalNode } from "../types";
 import type { TJsonApiBody } from "jsona/lib/JsonaTypes";
 
 // Get the Drupal base url
-export const baseURL: string = import.meta.env.DRUPAL_BASE_URL;
+export const baseUrl: string = import.meta.env.DRUPAL_BASE_URL;
 
 /**
  * Fetch url from Drupal
