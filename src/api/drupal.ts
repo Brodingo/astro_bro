f3589223a1f08c5b0d672901ed3b68786890ab31
Brodingo <brodyk@gmail.com>
2024-11-14 22:16:21 -0500

    setup handler for fetching and deserializing responses

--- src/api/drupal.ts
+++ src/api/drupal.ts
@@ -5,3 +5,17 @@ import type { TJsonApiBody } from "jsona/lib/JsonaTypes";
 
 // Get the Drupal base url
 export const baseURL: string = import.meta.env.DRUPAL_BASE_URL;
+
+/**
+ * Fetch url from Drupal
+ * 
+ * @param url
+ * 
+ * @return Promise<TJsonaModel | TJsonaModel[]> as Promise<any>
+ */
+export const fetchUurl = async (url: string): Promise<any> => {
+    const request: Response = await fetch(url);
+    const json: string | TJsonApiBody = await request.json();
+    const dataFormatter: Jsona = new Jsona();
+    return dataFormatter.deserialize(json);
+}
